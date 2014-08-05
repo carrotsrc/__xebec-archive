67e6403aeb26de89e2f9ee0516adcd89fae06e63
Charlie FG <charlie@carrotsrc.org>
2014-08-05 14:47:57 +0100

    init and display manager, includes the css

--- management/index.php
+++ management/index.php
@@ -4,13 +4,34 @@
 	$db = init_database();
 	init_session();
 
+	$manager = null;
+	$loaded = false;
+	if(isset($_GET['manager']))
+		$manager = "/".$_GET['manager'];
+
 	
+	$repo_config['manroot'] = dirname(__file__)."$manager";
+
+	if($manager != null) {
+		include($repo_config['manroot'].'/lib.php');
+		$loaded = true;
+	}
+	if($loaded)
+		init_manager($db);
+
 ?>
 
 <html>
 <head>
 <title>vegPatch Module Repository - Management Interface</title>
+<link href="../lib/main.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
+<?php
+	if(!$loaded)
+		die("Nothing selected");
+
+		echo display_manager($db);
+?>
 </body>
 </html>
