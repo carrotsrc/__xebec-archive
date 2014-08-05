0bbfb13f0691533556de1b20bd856ac38664d7a3
Charlie FG <charlie@carrotsrc.org>
2014-08-05 13:04:13 +0100

    Added management landing page

---
+++ management/index.php
@@ -0,0 +1,16 @@
+<?php
+	include('../system/init.php');
+	include('../lib/users.php');
+	$db = init_database();
+	init_session();
+
+	
+?>
+
+<html>
+<head>
+<title>vegPatch Module Repository - Management Interface</title>
+</head>
+<body>
+</body>
+</html>
