629c3638c000bd16cdddbf06d3444ab405440e13
Charlie FG <charlie@carrotsrc.org>
2014-08-05 21:54:58 +0100

    Implemented adding a package

--- system/collections/manage/new.php
+++ system/collections/manage/new.php
@@ -1,5 +1,32 @@
-Add Package
+<?php
+	// run the addition
+	$error = false;
+	$msg = null;
+	if(isset($_POST['action']) && $_POST['action'] == 'new') {
+		include('lib/packages.php');
+		include('lib/strings.php');
+		if(collection_routine_package_exists($_POST['name'], $collection['id'], $db)) {
+			$error = true;
+			$msg = "Package already exists in collection";
+		} else {
+			$id = package_db_add($_POST['name'], string_prepare_mysql($_POST['desc']), $db);
+			if(!$id) {
+				$error = true;
+				$msg = "Error creating package";
+			} else {
+				if(!collection_routine_add_package($id, $collection['id'], $db)) {
+					$error = true;
+					$msg = "Error adding package to collection";
+				} else 
+					$msg = "Successfully added package to collection";
+			}
+		}
+
+	}
+?>
+New Package
 <form method="post" action="">
+	<input type="hidden" name="action" value="new" />
 	<table>
 	<tr>
 		<td>Package Name:</td>
@@ -16,3 +43,12 @@ Add Package
 	</tr>
 	</table>
 </form>
+<?php
+if($error)
+	echo "<div class=\"color-error\">";
+else
+	echo "<div class=\"color-success\">";
+	echo $msg;
+	echo "</div>";
+
+?>
