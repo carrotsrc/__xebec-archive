e1bd8a5333e0a0843b6ad5df33ad283b579c17a7
Charlie FG <charlie@carrotsrc.org>
2014-08-05 18:47:09 +0100

    Added descriptive URL handling in index

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
 	include('system/init.php');
 	$db = init_database();
+	init_session();
+	$tokens = explode('/', $_SERVER['REQUEST_URI']);
+	if($tokens[1] == "" || $tokens[1] == "index.php")
+		$tokens[1] = "home";
+	$loaded = false;
+
+	if(!file_exists("system/".$tokens[1]."/main.php"))
+		$tokens = array('','errors','404');
+
+	include("system/".$tokens[1]."/main.php");
+	$title = "Home";
+	init_location($db, array_slice($tokens, 2), $title);
 ?>
 
 <html>
 <head>
-<title>vegPatch Module Repository</title>
+<title>vegPatch Module Repository - <?php echo $title; ?></title>
 </head>
 <body>
+<?php
+	display_location($db, array_slice($tokens, 2));
+?>
 </body>
 </html>
