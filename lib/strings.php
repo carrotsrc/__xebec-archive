1347518a8f6b83dea50019741a3e87df654cf02b
Charlie FG <charlie@carrotsrc.org>
2014-08-08 18:10:41 +0100

    Updated strings library for handling xml

--- lib/strings.php
+++ lib/strings.php
@@ -4,4 +4,15 @@
 		$string = str_replace("\"", "\\\"", $string);
 		return $string;
 	}
+
+	function string_prepare_xml($string)
+	{
+		$string = str_replace("<", "&lt;", $string);
+		$string = str_replace(">", "&gt;", $string);
+		$string = str_replace("&", "&amp;", $string);
+		$string = str_replace("'", "&apos;", $string);
+		$string = str_replace("\"", "&quot;", $string);
+
+		return $string;
+	}
 ?>
