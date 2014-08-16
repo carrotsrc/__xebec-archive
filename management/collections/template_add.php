b2da90b6363cfcd469e3dcf2bf49a430adf4b2d6
Charlie FG <charlie@carrotsrc.org>
2014-08-16 18:57:13 +0100

    Licensed and added correct readme

--- management/collections/template_add.php
+++ management/collections/template_add.php
@@ -1,3 +1,19 @@
+<?php
+/*
+* Copyright 2014, Zunautica Initiatives Ltd.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+
+?>
 <form action="" method="post">
 	<input type="text" name="collection" /></br >
 	<input type="hidden" name="action" value="add" />
